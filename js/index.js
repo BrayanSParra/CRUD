52c2f17b8fb8bc3aab62134c313fff864c33abec
BrayanSParra <110481985+BrayanSParra@users.noreply.github.com>
2023-10-05 19:53:23 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,6 +5,11 @@ function app(){
 	crud.create([1, 2, 3]);
 	crud.create({ message: "Hola mundo"});
 
+	crud.update(6, [3, 7, 1]);
+
+	crud.delete(4);
+
+
 	let crud2 = new CRUD("ejemplo");
 	console.log(crud2.readall());
 }
